e7c5d9c00fa5cb591dc1032b02ed290e02f381be
gusfeliciano <gustav213@gmail.com>
2024-08-06 17:01:09 -0600

    Added and updated cloud resume challenge part 1 blog post

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -9,24 +9,18 @@ export const metadata: Metadata = {
 
 const projects = [
   {
-    title: "Cloud Resume Challenge: Front-End",
-    description: "A brief description of project 1.",
-    technologies: ["React", "Node.js", "MongoDB"],
-    link: "https://project1.com"
-  },
-  {
-    title: "Project 2",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    technologies: ["Vue.js", "Express", "PostgreSQL"],
-    link: "https://project2.com"
-  },
-  {
-    title: "Project 3",
-    description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?",
-    technologies: ["Vue.js", "Express", "PostgreSQL"],
-    link: "https://project3.com"
+    title: "Cloud Resume Challenge Part 1: AWS Serverless Website",
+    description: "Built a cloud-native resume website using AWS services, showcasing skills in serverless architecture and cloud development. Implemented a visitor counter using a backend API.",
+    technologies: ["AWS S3", "CloudFront", "Route 53", "DynamoDB", "Lambda", "API Gateway", "HTML", "CSS", "JavaScript"],
+    link: "https://github.com/gusfeliciano/cloud-resume-frontend"
   },
   // Add more projects as needed
+  //{
+  //  title: "Project 2",
+  //  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  //  technologies: ["Vue.js", "Express", "PostgreSQL"],
+  //  link: "https://project2.com"
+  //},
 ]
 
 export default function Projects() {
